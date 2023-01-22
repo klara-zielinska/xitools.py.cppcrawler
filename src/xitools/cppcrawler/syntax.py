d1ba5f60b1394904500dc58e3867e62baad42cea
Klara Zielińska <clare.blackwater@gmail.com>
2023-01-22 22:17:09 +0100

    refining patterns + operator support

--- src/xitools/cppcrawler/syntax.py
+++ src/xitools/cppcrawler/syntax.py
@@ -3,16 +3,28 @@ import regex
 
 _commentRe  = r"//.*|/\*(?:[^\*]|\*[^/])*\*/"
 _commentPat = regex.compile(_commentRe)
-_stringRe   = r'"(?:[^"\\]|\r\n|\\.)*"'
+_stringRe   = r'"(?:[^"\\\n]|\\.)*"'
 _stringPat  = regex.compile(_stringRe)
-_charRe     = r"'(?:[^'\\]|\r\n|\\.)*'"
+_charRe     = r"'(?:[^'\\\n]|\\.)*'"
 _charPat    = regex.compile(_charRe)
-_sxRe       = r"\s|" + _commentRe # c++ space extra pattern (comments are white-spaces)
-_sxPat      = regex.compile(_sxRe)
 _clipRe     = f"{_commentRe}|{_stringRe}|{_charRe}"
 _clipPat    = regex.compile(_clipRe)
+_idRe = r"\b\w+\b"
+__  = f"(?:\\s|{_commentRe})"
+___ = f"{__}*+"
 _optValRe   = f"{_stringRe}|{_charRe}|[\w\.-]+"
 _optValPat  = regex.compile(_optValRe)
+
+_opSym0Re = r"\\({___}\\)|\\[{___}]"r"|,|[+\-\*/%^&|~!=<>]{1,3}"
+_ptrArgOps0Re = r"(?:\s*[&*]\s*|\s*const\b\s*)+"
+
+#def _opRe(*, macroVar=False): 
+#    macroVar = r"|\w+\b" if macroVar else ""
+#    return r"\boperator\b"f"{___}(?:\\({___}\\)|\\[{___}]"r"|,|[+\-\*/%^&|~!=<>]{1,3}"f"{macroVar}"r")"
+
+# Warning: can find an expression or an initializer
+_methFinderRe = (f"(?:{_idRe}{___}::{___})?"
+                 r"(?:~|\boperator\b"f"|{_idRe}(?:{___}<(?:{_clipRe}|[^;{{}}])*+>)?){___}"r"\(")
 
 _clipBeginPat   = regex.compile(r"['\"]|//|/\*|\r\n")
 _endCharPat     = regex.compile(r"'|\r\n|$")
@@ -20,11 +32,21 @@ _endStrPat      = regex.compile(r'"|\r\n|$')
 _endComment1Pat = regex.compile(r'\r\n|$')
 _endCommentNOrNlPat = regex.compile(r'\*/|\r\n|$')
 
-_methDec0Pat = regex.compile(r"(?:(\w+)\s*::\s*)?(~\s*)?(\w+)\s*\(\s*")
-_methDec1Pat = regex.compile(r"\s*(\w+)?(?P<opt>\s*=)?\s*")
-_methDec2Pat = regex.compile(r"\s*,?\s*")
-_methDec3Pat = regex.compile(r"\)(\s*const)?")
-_tpOps0Pat = regex.compile(r"(?!\s)(?:[\*&]|const\b|typename\b|\s+)*")
+
+_methProt0Pat = regex.compile(r"(?:(?P<cont>\w+)::)?")
+_methProt1Pat = regex.compile(r"(?:(?P<cont>\w+)::)?+(?:~)?+(?P<name>[^(]+)\(")
+_methDec0Pat = regex.compile(r"(?<!(?:,)\s*)(?:\b(?P<cont>\w+)\s*::\s*)?(?P<dest>~\s*)?"
+                             r"\b(?:operator\b\s*(?P<op>"f"(?:{_opSym0Re})?)"
+                                r"|(?P<name>\w+)\s*(?=(?P<x>\(|<)))")
+_methDec0aPat = regex.compile(f"\\({___}")
+_methDec1Pat = regex.compile(f"{___}"
+                             r"(?:\((?P<ptr>"f"{_ptrArgOps0Re}"r")(?P<name>\w+)?"f"{___}"r"\)"
+                                 r"|(?P<name>\w+)?)"
+                             f"{___}(?=(?P<fn>\())?")
+_methDec1aPat = regex.compile(f"{___}(?P<opt>=){___}")
+_methDec2Pat = regex.compile(f"{___},?{___}")
+_methDec3Pat = regex.compile(f"\\)({___}const)?")
+_tpOps0Pat = regex.compile(r"\s*((?:[\*&]|const\b|typename\b|\s+)*)")
 _tpOps1Pat = regex.compile(r"(?:[\*&]|const\b|\s+)*(?<!\s)")
 _tpSpaceRepPat = regex.compile(r"(\b\s+\b)|\s+")
 _tpBuiltInPat = regex.compile(r"(?:unsigned\s+)?(?:(?:short|long(?:\s+long)?)(?:\s+int)?|int|char)")
@@ -32,6 +54,7 @@ _tpName0Pat = regex.compile(r"\s*(\w+)")
 _tpName1Pat = regex.compile(r"\s*::\s*(\w+)")
 _tpArrPat = regex.compile(r"\s*\[[^]]*]")
 _tpTailPat = regex.compile(r"(?:\s*typename)?")
+_tpFuncPat = regex.compile(r"\s*(?:\((?P<ptr>"f"{_ptrArgOps0Re}"r")\)\s*)?(?=\()")
 _tempArgs0Pat = regex.compile(r"\s*<\s*")
 _tempArgs1Pat = regex.compile(r"\s*,\s*")
 _tempArgs2Pat = regex.compile(r"\s*>")
@@ -40,7 +63,7 @@ _indentPat = regex.compile(r"(?<=^|\r\n)([ \t]*+)")
 _expr0Pat = regex.compile(r"\s*+[^\(\)\[\],]+(?P<popen>\(|\[)?(?<!\s)")
 _expr1Pat = regex.compile(r"\s*,?\s*")
 _expr2Pat = regex.compile(r"[\)\]]")
-_classStructHead0Re = r"\b{keyword}\s+{name}\b\s*"
+_classStructHead0Re = r"\b({keyword})\s+({name})\b\s*"
 _classStructHead1Re = r"\s*(?::(?!:)(?:[^;\{]|"f"{_commentRe}"r")*+)?(?=\{)"
 
 
@@ -51,6 +74,16 @@ class Syntax:
     stringPat  = _stringPat
     charRe     = _charRe
     charPat    = _charPat
+    clipRe     = _clipRe
+    clipPat    = _clipPat
+    #opRe       = _opRe
+    methFinderRe = _methFinderRe
+
+
+    def methProtName(prot):
+        mres = _methProt1Pat.match(prot)
+        assert mres, prot
+        return mres.group("name")
 
     
     def unindent(code):
@@ -71,7 +104,7 @@ class Syntax:
     
 
     def makeNamespacePrefixRe(nsname):
-        return r"namespace\s+{}\b(?:\s|{})*{{".format(nsname, _commentRe)
+        return r"namespace\s+(?P<name>{})\b(?:\s|{})*{{".format(nsname, _commentRe)
 
 
     def makeDirectCallRe(methnames):
@@ -104,25 +137,24 @@ class Syntax:
     # - no comments
     def makeMethProtRe(prot):
         typeRe = r"[^\),]+"
-        protRe = r"(?:(\w+)::)?(~?\w+)\(({tp})?(?:, ({tp}))*\)( const)?".format(tp = typeRe)
+        protRe = (r"(?:(?P<cont>\w+)::)?(?P<name>operator\(\)|~?[^(]+)(?P<tempArgs><[^(]*>)?"
+                  r"\((?P<targ1>{tp})?(?:, (?P<targ2>{tp}))*\)(?P<const> const)?".format(tp = typeRe))
         mres = regex.fullmatch(protRe, prot)
-        if False: # to do
-            protRegex = r"(?<=(?:^|[;\{{}}])(?:{com}|\s)*)[\w\*&:][\w\*&:<>,\s]*?\s*\b".format(com = _commentRe)
-        else:
-            protRegex = "(?<!::\s*)"
-        if mres.group(1):
-            protRegex += f"{mres.group(1)}\\s*::\\s*"
-        protRegex += f"(?P<methname>{mres.group(2)})\\s*\(\\s*"
-        if mres.group(3): #
-            protRegex += (Syntax.makeTypeRe(mres.group(3)) + 
+        protRegex = "(?<!::\s*)"
+        if mres.group('cont'):
+            protRegex += f"{mres.group('cont')}\\s*::\\s*"
+        assert not mres.group('tempArgs'), "Template arguments currenly not supported"
+        protRegex += f"(?P<methname>{mres.group('name')})\\s*\(\\s*"
+        if mres.group('targ1'): #
+            protRegex += (Syntax.makeTypeRe(mres.group('targ1')) + 
                           r"\s*(?:\b(?P<argname>\w+)\s*)?(?:=\s*" f"(?:{_optValRe})\\s*)?")
-            for tp in mres.captures(4):
+            for tp in mres.captures('targ2'):
                 protRegex += (r",\s*" + Syntax.makeTypeRe(tp) + 
                               r"\s*(?:\b(?P<argname>\w+)\s*)?(?:=\s*" f"(?:{_optValRe})\\s*)?")
         else:
             protRegex += "(?P<argname>a^)?"
         protRegex += r"\)"
-        if mres.group(5):
+        if mres.group('const'):
             protRegex += r"\s*const"
         return protRegex
 
@@ -151,7 +183,7 @@ class Syntax:
 
     def parseType(s, begin=0):
         mres = _tpOps0Pat.match(s, begin)
-        tp = mres.group(0)
+        tp = mres.group(1)
         pos = mres.end()
         
         if mres := _tpBuiltInPat.match(s, pos):
@@ -183,9 +215,16 @@ class Syntax:
         
         mres = _tpOps1Pat.match(s, pos)
         tp += mres.group(0)
+        pos = mres.end()
+
+        if mres := _tpFuncPat.match(s, pos):
+            return None
+            #if ptr := mres.group("ptr"): tp += "{"f"{ptr}""}"
+            #(targs, nargs, mods, pos) = Syntax.parseMethDecTail(s, mres.end())
+            #tp += "{" + "`".join(targs) + "}" + "%".join(mods)
 
         tp = _tpSpaceRepPat.sub(lambda m: "%" if m.group(1) else "", tp)
-        return (tp, mres.end())
+        return (tp, pos)
 
 
     def parseExpr(s, begin=0):
@@ -219,43 +258,76 @@ class Syntax:
         return (expr, begin)
 
 
-    def parseMethDeclaration(s, begin=0):
-        mres = _methDec0Pat.match(s, begin)
-        if not mres: return None
-
-        contName = mres.group(1)
-        destru = mres.group(2)
-        name = mres.group(3)
-        begin = mres.end()
-
+    def parseMethDecTail(s, begin=0):
+        mres = _methDec0aPat.match(s, begin)
+        pos = mres.end()
         targs = []
         nargs = []
-        if s[mres.end()] != ')':
-            def readArg(pos):
-                match Syntax.parseType(s, pos):
-                    case (tp, pos):
-                        targs.append(tp)
-                        mres2 = _methDec1Pat.match(s, pos)
-                        nargs.append(mres2.group(1))
-                        pos = mres2.end()
-                        if mres2.group("opt") and (res := Syntax.parseExpr(s, pos)):
-                            pos = res[1]
-                        mres2 = _methDec2Pat.match(s, pos)
-                        return (mres2.end(), None)
-                    case None:
+        def readArg(pos):
+            match Syntax.parseType(s, pos):
+                case None:
+                    return None
+
+                case (tp, pos):
+                    mres2 = _methDec1Pat.match(s, pos)
+                    name = mres2.group("name")
+                    if ptr := mres2.group("ptr"):
+                        ptr = _tpSpaceRepPat.sub(lambda m: "%" if m.group(1) else "", ptr)
+                    else:
+                        ptr = ""
+                    pos = mres2.end()
+                    if mres2.group("fn"):
                         return None
-            while (res := readArg(begin)): begin = res[0]
+                        #if ptr: tp += "{"f"{ptr}""}"
+                        #match Syntax.parseMethDecTail(s, pos):
+                        #    case None: return None
+                        #    case (targs2, nargs2, mods2, pos):
+                        #        tp += "{"f"{'`'.join(targs2)}""}" + "%".join(mods2)
+                    targs.append(tp)
+                    nargs.append(name)
+                    if (mres2 := _methDec1aPat.match(s, pos)) and (exppos := Syntax.parseExpr(s, mres2.end())):
+                        pos = exppos[1]
+                    mres2 = _methDec2Pat.match(s, pos)
+                    return (mres2.end(), None)
+        while (res := readArg(pos)): pos = res[0]
 
-        mres = _methDec3Pat.match(s, begin)
+        mres = _methDec3Pat.match(s, pos)
         if not mres: return None
-        const = mres.group(1) is not None
-        
-        prot = contName + "::" if contName else "" 
-        prot += "~" if destru else ""
-        prot += name + "(" + ", ".join(targs) + ")"
-        if const: prot += " const"
-        return (prot, nargs, mres.end())
+        mods = ["const"] if mres.group(1) else []
 
+        return (targs, nargs, mods, mres.end())
+
+
+    def parseMethPrototype(s, begin=0):
+        try:
+            mres = _methDec0Pat.match(s, begin)
+            if not mres: return None
+
+            contName = mres.group("cont")
+            dest = mres.group("dest")
+
+            if op := mres.group("op"):
+                id = f"operator{op}"
+            elif (pos := mres.end("op")) >= 0:
+                match Syntax.parseType(s, pos):
+                    case (tp, pos): id = f"operator {tp}"
+                    case None: return None
+            else:
+                pos = mres.end()
+                match Syntax.parseTempArgs(s, pos):
+                    case (tempArgs, pos): id = mres.group("name") + tempArgs
+                    case None:            id = mres.group("name")
+
+            (targs, nargs, mods, pos) = Syntax.parseMethDecTail(s, pos)
+        
+            prot = contName + "::" if contName else "" 
+            prot += "~" if dest else ""
+            prot += f"{id}({', '.join(targs)})"
+            if mods: prot += " " + " ".join(mods)
+            return (prot, nargs, pos)
+
+        except TypeError:
+            return None
 
     def parseClassStructPrefix(kind, name, s, begin=0):
         assert kind in ["class", "struct", None]
@@ -269,16 +341,16 @@ class Syntax:
         match Syntax.parseTempArgs(s, pos):
             case (_, pos): pass
 
-        mres = regex.match(_classStructHead1Re, s, pos=pos)
-        if mres: return (s[begin:mres.end()], mres.end())
+        mres2 = regex.match(_classStructHead1Re, s, pos=pos)
+        if mres2: return (mres.group(1), mres.group(2), mres2.end())
         else:    return None
 
     
     # prot in normal form (see above)
     # no template specialization support (e.g. A<std::vector<int>>::foo() )
     def extractProtContainerName(prot):
-        mres = _methDec0Pat.match(prot)
-        return (mres.group(1), prot[mres.start(3):])
+        mres = _methProt0Pat.match(prot)
+        return (mres.group("cont"), prot[mres.end():])
     
 
     def tempPar(n):
