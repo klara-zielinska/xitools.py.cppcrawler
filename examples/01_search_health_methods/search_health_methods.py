34af7b936f6f8db564926b87ac24769dc5ff2e31
Klara Zielińska <clare.blackwater@gmail.com>
2023-02-28 10:11:37 +0100

    example refactor

--- examples/01_search_health_methods/search_health_methods.py
+++ examples/01_search_health_methods/search_health_methods.py
@@ -5,12 +5,12 @@ methSearchRe = r"(?i)\b(?P<name>\w*health\w*)\s*\("
 for src in crawler.loadSourceFiles(["*.h", "*.cpp"]):
     if src.tryScopeToClassBody("class|struct", r"Cv\w*Info"):
         found = []
-        for (rmatch, stag) in src.findAll(methSearchRe, scopeTag=True, skipBlocks=True):
-            if Syntax.parseMethPrototype(src.intCode(rmatch.start())):
-                found.append((rmatch, stag))
+        for (mch, tag) in src.findAll(methSearchRe, scopeTag=True, skipBlocks=True):
+            if Syntax.parseMethPrototype(src.intCode(), mch.start()):
+                found.append((mch, tag))
 
         if found:
             print("\n\n" + src.filepath() + "\n---")
-            for rmatch, (kind, name) in found:
-                print(f'({kind}) {name}::{rmatch.group("name")}', rmatch.startLoc())
+            for mch, (kind, name) in found:
+                print(f'({kind}) {name}::{mch.group("name")}', mch.startLoc())
                 
